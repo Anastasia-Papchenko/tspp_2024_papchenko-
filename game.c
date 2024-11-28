094a4d840c86ea546ae4a3a416c74b2c000d5478
Anastasia-Papchenko <papchenko30@gmail.com>
2024-11-29 00:46:06 +0300

    неблокирующие операторы

--- game.c
+++ game.c
@@ -3,34 +3,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <stdbool.h>
 
 #define ALIVE 1
 #define DEAD 0
 
-
-void initialize_grid(int *grid, int rows, int cols) {
+void initialize_grid(int *grid, int rows, int cols, int rank, int size) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            grid[i * cols + j] = rand() % 2; 
+            grid[i * cols + j] = (i == rank && j == 2) ? ALIVE : DEAD; 
         }
     }
 }
 
+void exchange_borders(int *local_grid, int local_rows, int cols, int rank, int size, MPI_Request *requests) {
+    int top_rank = (rank == 0) ? size - 1 : rank - 1;
+    int bottom_rank = (rank == size - 1) ? 0 : rank + 1;
+
+    MPI_Isend(&local_grid[0], cols, MPI_INT, top_rank, 0, MPI_COMM_WORLD, &requests[0]);
+    MPI_Isend(&local_grid[(local_rows - 1) * cols], cols, MPI_INT, bottom_rank, 1, MPI_COMM_WORLD, &requests[1]);
+    MPI_Irecv(&local_grid[-cols], cols, MPI_INT, top_rank, 1, MPI_COMM_WORLD, &requests[2]);
+    MPI_Irecv(&local_grid[local_rows * cols], cols, MPI_INT, bottom_rank, 0, MPI_COMM_WORLD, &requests[3]);
+}
 
 int count_neighbors(int *grid, int rows, int cols, int x, int y) {
     int count = 0;
-    for (int i = -1; i <= 1; ++i) {
-        for (int j = -1; j <= 1; ++j) {
-            if (!(i == 0 && j == 0)) {
-                int ni = (x + i + rows) % rows; 
-                int nj = (y + j + cols) % cols;
-                count += grid[ni * cols + nj];
-            }
+    for (int dx = -1; dx <= 1; ++dx) {
+        for (int dy = -1; dy <= 1; ++dy) {
+            if (dx == 0 && dy == 0) continue;
+            int nx = (x + dx + rows) % rows;
+            int ny = (y + dy + cols) % cols;
+            count += grid[nx * cols + ny];
         }
     }
     return count;
 }
-
 
 void update_grid(int *grid, int *new_grid, int rows, int cols) {
     for (int i = 0; i < rows; ++i) {
@@ -43,6 +50,10 @@ void update_grid(int *grid, int *new_grid, int rows, int cols) {
             }
         }
     }
+}
+
+bool check_stability(int current_alive, int prev_alive) {
+    return current_alive == prev_alive;
 }
 
 int main(int argc, char *argv[]) {
@@ -60,74 +71,69 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    int rows = atoi(argv[1]);     
-    int cols = atoi(argv[2]);     
-    int iterations = atoi(argv[3]); 
-    int local_rows = rows / size; 
+    int rows = atoi(argv[1]);
+    int cols = atoi(argv[2]);
+    int max_iterations = atoi(argv[3]);
 
-    int *grid = NULL;
+    int local_rows = rows / size + (rank < rows % size ? 1 : 0);
+    int *local_grid = (int *)malloc((local_rows + 2) * cols * sizeof(int)); 
     int *new_grid = (int *)malloc(local_rows * cols * sizeof(int));
-    int *local_grid = (int *)malloc(local_rows * cols * sizeof(int));
 
-   
-    if (rank == 0) {
-        grid = (int *)malloc(rows * cols * sizeof(int));
-        initialize_grid(grid, rows, cols);
-    }
+    initialize_grid(&local_grid[cols], local_rows, cols, rank, size);
 
-    
-    MPI_Scatter(grid, local_rows * cols, MPI_INT, local_grid, local_rows * cols, MPI_INT, 0, MPI_COMM_WORLD);
-
-    int game_active = 1;
-    int global_alive_count = 0;
+    MPI_Barrier(MPI_COMM_WORLD);
 
     struct timeval start, end;
     gettimeofday(&start, NULL);
 
-    for (int iteration = 0; iteration < iterations && game_active; ++iteration) {
-        
-        update_grid(local_grid, new_grid, local_rows, cols);
+    MPI_Request requests[4];
+    int global_alive = 0, local_alive, prev_local_alive = -1;
 
-        
-        int local_alive_count = 0;
+    for (int iter = 0; iter < max_iterations; ++iter) {
+      
+        exchange_borders(&local_grid[cols], local_rows, cols, rank, size, requests);
+        MPI_Waitall(4, requests, MPI_STATUSES_IGNORE);
+
+  
+        update_grid(&local_grid[cols], new_grid, local_rows, cols);
+
+      
+        local_alive = 0;
         for (int i = 0; i < local_rows; ++i) {
             for (int j = 0; j < cols; ++j) {
-                if (local_grid[i * cols + j] == ALIVE) {
-                    local_alive_count++;
+                if (new_grid[i * cols + j] == ALIVE) {
+                    local_alive++;
                 }
             }
         }
 
-        
-        MPI_Allreduce(&local_alive_count, &global_alive_count, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-
-        
-        static int previous_alive_count = -1;
-        if (previous_alive_count == global_alive_count) {
-            game_active = 0;
+       
+        if (check_stability(local_alive, prev_local_alive)) {
+            break;
         }
-        previous_alive_count = global_alive_count;
+        prev_local_alive = local_alive;
 
-        
         int *temp = local_grid;
         local_grid = new_grid;
         new_grid = temp;
+
+        MPI_Allreduce(&local_alive, &global_alive, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+        if (global_alive == 0) break;
     }
 
     gettimeofday(&end, NULL);
     double elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
 
     if (rank == 0) {
-        printf("Total number of living cells: %d\n", global_alive_count);
-        printf("Execution time: %f sec\n", elapsed_time);
+        printf("Total number of living cells: %d\n", global_alive);
+        printf("Execution time: %f seconds\n", elapsed_time);
     }
 
-    free(new_grid);
     free(local_grid);
-    if (rank == 0) {
-        free(grid);
-    }
+    free(new_grid);
 
     MPI_Finalize();
     return 0;
 }
+
